22a325871bd02d62a0efbdf3c887b6883c94900a
Cat Core <arthur.koalas@free.fr>
2022-04-13 18:26:59 +0200

    port EntityRenderDispatcher patch

---
+++ src/main/java/fr/catcore/fabricatedforge/mixin/modloader/client/EntityRenderDispatcherMixin.java
@@ -0,0 +1,22 @@
+package fr.catcore.fabricatedforge.mixin.modloader.client;
+
+import modloader.ModLoader;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
+
+@Mixin(EntityRenderDispatcher.class)
+public class EntityRenderDispatcherMixin {
+
+    @Shadow private Map renderers;
+
+    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Ljava/util/Map;values()Ljava/util/Collection;", remap = false))
+    private void modLoaderAddAllRenderers(CallbackInfo ci) {
+        ModLoader.addAllRenderers(this.renderers);
+    }
+}
