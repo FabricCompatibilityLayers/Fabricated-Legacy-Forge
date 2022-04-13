ba981750df0a1c4d3cd4b96c2bfc4307e5ea19e9
Cat Core <arthur.koalas@free.fr>
2022-04-13 19:51:28 +0200

    port FurnaceOutputSlot patch

---
+++ src/main/java/fr/catcore/fabricatedforge/mixin/modloader/common/FurnaceOutputSlotMixin.java
@@ -0,0 +1,22 @@
+package fr.catcore.fabricatedforge.mixin.modloader.common;
+
+import modloader.ModLoader;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.slot.FurnaceOutputSlot;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(FurnaceOutputSlot.class)
+public class FurnaceOutputSlotMixin {
+
+    @Shadow private PlayerEntity player;
+
+    @Inject(method = "onCrafted(Lnet/minecraft/item/ItemStack;)V", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/item/ItemStack;onCraft(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;I)V"))
+    private void modLoaderTakenFromFurnace(ItemStack par1, CallbackInfo ci) {
+        ModLoader.takenFromFurnace(this.player, par1);
+    }
+}
