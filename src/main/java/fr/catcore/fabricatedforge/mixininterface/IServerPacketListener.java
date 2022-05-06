274d34d75dbff3a40e1610e971e3cc6b03ff8123
Cat Core <arthur.koalas@free.fr>
2022-05-06 22:05:33 +0200

    remove modloader code

--- src/main/java/fr/catcore/fabricatedforge/mixininterface/IServerPacketListener.java
+++
@@ -1,7 +0,0 @@
-package fr.catcore.fabricatedforge.mixininterface;
-
-import net.minecraft.entity.player.ServerPlayerEntity;
-
-public interface IServerPacketListener {
-    ServerPlayerEntity getPlayer();
-}
