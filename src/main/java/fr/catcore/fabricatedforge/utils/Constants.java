e9a23e932de957f72c7f044b27a9b4270d4803e0
Cat Core <arthur.koalas@free.fr>
2022-05-21 01:32:11 +0200

    versionned cache folder

--- src/main/java/fr/catcore/fabricatedforge/utils/Constants.java
+++ src/main/java/fr/catcore/fabricatedforge/utils/Constants.java
@@ -7,7 +7,11 @@ import java.io.File;
 
 public class Constants {
     public static final File MAIN_FOLDER = new File(FabricLoader.getInstance().getGameDir().toFile(), "fabricated-forge");
-    public static final File VERSIONED_FOLDER = new File(MAIN_FOLDER, FabricLoader.getInstance().getModContainer("minecraft").get().getMetadata().getVersion().getFriendlyString());
+    public static final File VERSIONED_FOLDER = new File(
+            new File(MAIN_FOLDER,
+                FabricLoader.getInstance().getModContainer("minecraft").get().getMetadata().getVersion().getFriendlyString()
+            ), FabricLoader.getInstance().getModContainer("fabricated-forge").get().getMetadata().getVersion().getFriendlyString()
+    );
     public static final File MAPPINGS_FILE = new File(VERSIONED_FOLDER, "mappings.tiny");
     public static final File MOD_MAPPINGS_FILE = new File(VERSIONED_FOLDER, "mods_mappings.tiny");
     public static final File REMAPPED_FOLDER = new File(VERSIONED_FOLDER, "mods");
