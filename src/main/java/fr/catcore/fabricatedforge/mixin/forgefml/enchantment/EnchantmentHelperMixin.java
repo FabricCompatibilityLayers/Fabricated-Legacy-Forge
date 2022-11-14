f4bcb3d46c8d722706f08de5eade0df595ae012d
Cat Core <arthur.koalas@free.fr>
2022-11-14 17:20:35 +0100

    Enchantment Patches Done

--- src/main/java/fr/catcore/fabricatedforge/mixin/forgefml/enchantment/EnchantmentHelperMixin.java
+++ src/main/java/fr/catcore/fabricatedforge/mixin/forgefml/enchantment/EnchantmentHelperMixin.java
@@ -20,15 +20,15 @@ public class EnchantmentHelperMixin {
      */
     @Overwrite
     public static Map method_3528(int par0, ItemStack par1ItemStack) {
-        HashMap<Integer, EnchantmentLevelEntry> var3 = null;
-        Enchantment[] var4 = Enchantment.ALL_ENCHANTMENTS;
+        Item var2 = par1ItemStack.getItem();
+        HashMap var3 = null;
 
-        for (Enchantment var7 : var4) {
+        for(Enchantment var7 : Enchantment.ALL_ENCHANTMENTS) {
             if (var7 != null && var7.canEnchantItem(par1ItemStack)) {
-                for (int var8 = var7.getMinimumLevel(); var8 <= var7.getMaximumLevel(); ++var8) {
+                for(int var8 = var7.getMinimumLevel(); var8 <= var7.getMaximumLevel(); ++var8) {
                     if (par0 >= var7.getMinimumPower(var8) && par0 <= var7.getMaximumPower(var8)) {
                         if (var3 == null) {
-                            var3 = new HashMap<>();
+                            var3 = new HashMap();
                         }
 
                         var3.put(var7.id, new EnchantmentLevelEntry(var7, var8));
