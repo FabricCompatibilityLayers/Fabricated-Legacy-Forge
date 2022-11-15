ede656d8a8e0a5156fcd88f0b2609222dc4eddd0
Cat Core <arthur.koalas@free.fr>
2022-11-15 17:29:37 +0100

    Recipe Patches Done

--- src/main/java/fr/catcore/fabricatedforge/mixin/forgefml/recipe/RecipeDispatcherMixin.java
+++ src/main/java/fr/catcore/fabricatedforge/mixin/forgefml/recipe/RecipeDispatcherMixin.java
@@ -1,64 +1,62 @@
 package fr.catcore.fabricatedforge.mixin.forgefml.recipe;
 
-import fr.catcore.fabricatedforge.mixininterface.IItem;
 import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.recipe.RecipeDispatcher;
 import net.minecraft.recipe.RecipeType;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
-import java.util.Iterator;
 import java.util.List;
 
 @Mixin(RecipeDispatcher.class)
 public class RecipeDispatcherMixin {
 
-    @Shadow private List recipes;
+    @Shadow private List<RecipeType> recipes;
 
     /**
      * @author Minecraft Forge
      * @reason none
      */
     @Overwrite
-    public ItemStack method_3494(CraftingInventory par1InventoryCrafting) {
-        int var2 = 0;
-        ItemStack var3 = null;
+    public ItemStack matches(CraftingInventory par1InventoryCrafting, World par2World) {
+        int var3 = 0;
         ItemStack var4 = null;
+        ItemStack var5 = null;
 
-        for(int var5 = 0; var5 < par1InventoryCrafting.getInvSize(); ++var5) {
-            ItemStack var6 = par1InventoryCrafting.getInvStack(var5);
-            if (var6 != null) {
-                if (var2 == 0) {
-                    var3 = var6;
+        for(int var6 = 0; var6 < par1InventoryCrafting.getInvSize(); ++var6) {
+            ItemStack var7 = par1InventoryCrafting.getInvStack(var6);
+            if (var7 != null) {
+                if (var3 == 0) {
+                    var4 = var7;
                 }
 
-                if (var2 == 1) {
-                    var4 = var6;
+                if (var3 == 1) {
+                    var5 = var7;
                 }
 
-                ++var2;
+                ++var3;
             }
         }
 
-        if (var2 == 2 && var3.id == var4.id && var3.count == 1 && var4.count == 1 && ((IItem)Item.ITEMS[var3.id]).isRepairable()) {
-            Item var10 = Item.ITEMS[var3.id];
-            int var12 = var10.getMaxDamage() - var3.getDamage();
-            int var7 = var10.getMaxDamage() - var4.getDamage();
-            int var8 = var12 + var7 + var10.getMaxDamage() * 10 / 100;
-            int var9 = var10.getMaxDamage() - var8;
-            if (var9 < 0) {
-                var9 = 0;
+        if (var3 == 2 && var4.id == var5.id && var4.count == 1 && var5.count == 1 && Item.ITEMS[var4.id].isRepairable()) {
+            Item var13 = Item.ITEMS[var4.id];
+            int var14 = var13.getMaxDamage() - var4.getDamage();
+            int var8 = var13.getMaxDamage() - var5.getDamage();
+            int var9 = var14 + var8 + var13.getMaxDamage() * 5 / 100;
+            int var10 = var13.getMaxDamage() - var9;
+            if (var10 < 0) {
+                var10 = 0;
             }
 
-            return new ItemStack(var3.id, 1, var9);
+            return new ItemStack(var4.id, 1, var10);
         } else {
-            for (Object recipe : this.recipes) {
-                RecipeType var13 = (RecipeType) recipe;
-                if (var13.method_3500(par1InventoryCrafting)) {
-                    return var13.getResult(par1InventoryCrafting);
+            for(RecipeType var12 : this.recipes) {
+                if (var12.matches(par1InventoryCrafting, par2World)) {
+                    return var12.getResult(par1InventoryCrafting);
                 }
             }
 
