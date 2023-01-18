d9726b7a9cd3ed02e60d1a2d78d5cd2147d6cf34
Cat Core <arthur.koalas@free.fr>
2023-01-19 00:25:34 +0100

    apply license headers

--- src/main/java/net/minecraftforge/client/event/sound/SoundLoadEvent.java
+++ src/main/java/net/minecraftforge/client/event/sound/SoundLoadEvent.java
@@ -1,3 +1,7 @@
+/**
+ * This software is provided under the terms of the Minecraft Forge Public
+ * License v1.0.
+ */
 package net.minecraftforge.client.event.sound;
 
 import net.minecraft.client.sound.SoundSystem;
