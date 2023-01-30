8c10dc45ecabc420c4eb3265f8b7cb7537b7a133
Cat Core <arthur.koalas@free.fr>
2023-01-30 16:45:10 +0100

    Finish port

--- src/main/java/fr/catcore/fabricatedforge/Constants.java
+++ src/main/java/fr/catcore/fabricatedforge/Constants.java
@@ -3,7 +3,7 @@ package fr.catcore.fabricatedforge;
 import java.io.File;
 
 public class Constants {
-    public static final String FORGE_URL = "https://maven.minecraftforge.net/net/minecraftforge/forge/1.4.4-6.3.0.378/forge-1.4.4-6.3.0.378-universal.zip";
+    public static final String FORGE_URL = "https://maven.minecraftforge.net/net/minecraftforge/forge/1.4.5-6.4.2.448/forge-1.4.5-6.4.2.448-universal.zip";
 
     public static final File MODS_FOLDER = new File(fr.catcore.modremapperapi.utils.Constants.VERSIONED_FOLDER, "mods");
     public static final File COREMODS_FOLDER = new File(fr.catcore.modremapperapi.utils.Constants.VERSIONED_FOLDER, "coremods");
