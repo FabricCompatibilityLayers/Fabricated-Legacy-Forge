cbe733c1e3a1479446b19ab9bfeb14fa77f2dc35
Cat Core <arthur.koalas@free.fr>
2023-01-30 21:25:40 +0100

    use the same fix for WeightedRandomChestContent

--- src/main/java/fr/catcore/fabricatedforge/mixin/FabricatedForgeMixinPlugin.java
+++ src/main/java/fr/catcore/fabricatedforge/mixin/FabricatedForgeMixinPlugin.java
@@ -71,44 +71,85 @@ public class FabricatedForgeMixinPlugin implements IMixinConfigPlugin {
             case "net.minecraft.class_588":
             case "net.minecraft.class_589":
             case "net.minecraft.class_590":
-                for (MethodNode node : targetClass.methods) {
-                    if (Objects.equals(node.name, "<init>")) {
-                        for(AbstractInsnNode insNode : node.instructions) {
-                            if (insNode instanceof MethodInsnNode && insNode.getOpcode() == INVOKESPECIAL) {
-                                MethodInsnNode mTheNode = (MethodInsnNode) insNode;
-                                if (Objects.equals(mTheNode.owner, "net/minecraft/class_584")) mTheNode.owner = "cpw/mods/fml/client/FMLTextureFX";
+                {
+                    for (MethodNode node : targetClass.methods) {
+                        if (Objects.equals(node.name, "<init>")) {
+                            for (AbstractInsnNode insNode : node.instructions) {
+                                if (insNode instanceof MethodInsnNode && insNode.getOpcode() == INVOKESPECIAL) {
+                                    MethodInsnNode mTheNode = (MethodInsnNode) insNode;
+                                    if (Objects.equals(mTheNode.owner, "net/minecraft/class_584"))
+                                        mTheNode.owner = "cpw/mods/fml/client/FMLTextureFX";
+                                }
                             }
                         }
                     }
                 }
                 break;
             case "net.minecraft.class_1041":
-                // getNextID
-                MethodVisitor getNextIDVisitor = targetClass.visitMethod(ACC_PUBLIC + ACC_STATIC, "getNextID", "()I", null, null);
-                getNextIDVisitor.visitFieldInsn(GETSTATIC, "net/minecraft/class_1041", "field_4173", "[Lnet/minecraft/class_1041;");
-                getNextIDVisitor.visitInsn(ARRAYLENGTH);
-                getNextIDVisitor.visitInsn(IRETURN);
-                getNextIDVisitor.visitMaxs(1, 0);
-                getNextIDVisitor.visitEnd();
+                {
+                    // getNextID
+                    MethodVisitor getNextIDVisitor = targetClass.visitMethod(ACC_PUBLIC + ACC_STATIC, "getNextID", "()I", null, null);
+                    getNextIDVisitor.visitFieldInsn(GETSTATIC, "net/minecraft/class_1041", "field_4173", "[Lnet/minecraft/class_1041;");
+                    getNextIDVisitor.visitInsn(ARRAYLENGTH);
+                    getNextIDVisitor.visitInsn(IRETURN);
+                    getNextIDVisitor.visitMaxs(1, 0);
+                    getNextIDVisitor.visitEnd();
 
-                // <init>
-                MethodVisitor initVisitor = targetClass.visitMethod(ACC_PUBLIC, "<init>", "(Ljava/lang/String;)V", null, null);
-                Label l0 = new Label();
-                initVisitor.visitLabel(l0);
-                initVisitor.visitVarInsn(ALOAD, 0);
-                initVisitor.visitMethodInsn(INVOKESTATIC, "net/minecraft/class_1041", "getNextID", "()I", false);
-                initVisitor.visitVarInsn(ALOAD, 1);
-                initVisitor.visitMethodInsn(INVOKESPECIAL, "net/minecraft/class_1041", "<init>", "(ILjava/lang/String;)V", false);
-                Label l1 = new Label();
-                initVisitor.visitLabel(l1);
-                initVisitor.visitInsn(RETURN);
-                Label l2 = new Label();
-                initVisitor.visitLabel(l2);
-                initVisitor.visitLocalVariable("this", "Lnet/minecraft/class_1041;", null, l0, l2, 0);
-                initVisitor.visitLocalVariable("label", "Ljava/lang/String;", null, l0, l2, 1);
-                initVisitor.visitMaxs(3, 2);
-                initVisitor.visitEnd();
-
+                    // <init>
+                    MethodVisitor initVisitor = targetClass.visitMethod(ACC_PUBLIC, "<init>", "(Ljava/lang/String;)V", null, null);
+                    Label l0 = new Label();
+                    initVisitor.visitLabel(l0);
+                    initVisitor.visitVarInsn(ALOAD, 0);
+                    initVisitor.visitMethodInsn(INVOKESTATIC, "net/minecraft/class_1041", "getNextID", "()I", false);
+                    initVisitor.visitVarInsn(ALOAD, 1);
+                    initVisitor.visitMethodInsn(INVOKESPECIAL, "net/minecraft/class_1041", "<init>", "(ILjava/lang/String;)V", false);
+                    Label l1 = new Label();
+                    initVisitor.visitLabel(l1);
+                    initVisitor.visitInsn(RETURN);
+                    Label l2 = new Label();
+                    initVisitor.visitLabel(l2);
+                    initVisitor.visitLocalVariable("this", "Lnet/minecraft/class_1041;", null, l0, l2, 0);
+                    initVisitor.visitLocalVariable("label", "Ljava/lang/String;", null, l0, l2, 1);
+                    initVisitor.visitMaxs(3, 2);
+                    initVisitor.visitEnd();
+                }
+                break;
+            case "net.minecraft.class_847":
+                {
+                    // <init>
+                    MethodVisitor initVisitor = targetClass.visitMethod(ACC_PUBLIC, "<init>", "(Lnet/minecraft/class_1071;III)V", null, null);
+                    Label l0 = new Label();
+                    initVisitor.visitLabel(l0);
+                    initVisitor.visitVarInsn(ALOAD, 0);
+                    initVisitor.visitVarInsn(ILOAD, 4);
+                    initVisitor.visitMethodInsn(INVOKESPECIAL, "net/minecraft/class_846", "<init>", "(I)V", false);
+                    Label l1 = new Label();
+                    initVisitor.visitLabel(l1);
+                    initVisitor.visitVarInsn(ALOAD, 0);
+                    initVisitor.visitVarInsn(ALOAD, 1);
+                    initVisitor.visitFieldInsn(PUTFIELD, "net/minecraft/class_847", "itemStack", "Lnet/minecraft/class_1071;");
+                    Label l2 = new Label();
+                    initVisitor.visitLabel(l2);
+                    initVisitor.visitVarInsn(ALOAD, 0);
+                    initVisitor.visitVarInsn(ILOAD, 2);
+                    initVisitor.visitFieldInsn(PUTFIELD, "net/minecraft/class_847", "field_3107", "I");
+                    Label l3 = new Label();
+                    initVisitor.visitLabel(l3);
+                    initVisitor.visitVarInsn(ALOAD, 0);
+                    initVisitor.visitVarInsn(ILOAD, 2);
+                    initVisitor.visitFieldInsn(PUTFIELD, "net/minecraft/class_847", "field_3108", "I");
+                    Label l4 = new Label();
+                    initVisitor.visitLabel(l4);
+                    initVisitor.visitInsn(RETURN);
+                    Label l5 = new Label();
+                    initVisitor.visitLocalVariable("this", "Lnet/minecraft/class_847;", null, l0, l5, 0);
+                    initVisitor.visitLocalVariable("stack", "Lnet/minecraft/class_1071;", null, l0, l5, 1);
+                    initVisitor.visitLocalVariable("min", "I", null, l0, l5, 2);
+                    initVisitor.visitLocalVariable("max", "I", null, l0, l5, 3);
+                    initVisitor.visitLocalVariable("weight", "I", null, l0, l5, 4);
+                    initVisitor.visitMaxs(2, 5);
+                    initVisitor.visitEnd();
+                }
                 break;
             default:
                 break;
