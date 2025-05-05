0203bdd2e1ec920db08ab157b7954ee13aa2d802
Cat Core <arthur.koalas@free.fr>
2025-05-05 18:42:41 +0200

    Start working on Minecraft Forge

---
+++ fabricated-forge/src/main/java/io/github/fabriccompatibilitylayers/fabricatedforge/FabricatedForgeMixinPlugin.java
@@ -0,0 +1,82 @@
+package io.github.fabriccompatibilitylayers.fabricatedforge;
+
+import fr.catcore.cursedmixinextensions.CursedMixinExtensions;
+import fr.catcore.wfvaio.FabricVariants;
+import fr.catcore.wfvaio.WhichFabricVariantAmIOn;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.impl.launch.FabricLauncherBase;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+public class FabricatedForgeMixinPlugin implements IMixinConfigPlugin {
+
+    @Override
+    public void onLoad(String mixinPackage) {
+
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    private static List<String> OPTIFINE_OVERRIDES = Arrays.asList(
+            "anz",
+            "aob",
+            "aov",
+            "apx",
+            "aus",
+            "auw",
+            "avb",
+            "avc",
+            "ave",
+            "avf",
+            "avg",
+            "avi",
+            "avk",
+            "avl",
+            "ik",
+            "jw"
+    );
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if (FabricLoader.getInstance().isModLoaded("optifabric")) {
+            if (OPTIFINE_OVERRIDES.contains(FabricLoader.getInstance().getMappingResolver()
+                    .unmapClassName("official", targetClassName)) && !mixinClassName.endsWith("Accessor") && !mixinClassName.contains(".optifine.")) {
+                System.out.println("[Fabricated-Legacy-Forge] Mixin cancelled for Optifine compatibility: " + mixinClassName);
+                return false;
+            }
+
+            return true;
+        } else {
+            return !mixinClassName.contains(".optifine.");
+        }
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+        CursedMixinExtensions.postApply(targetClass);
+    }
+}
---
+++ fabricated-forge/src/main/java/io/github/fabriccompatibilitylayers/fabricatedforge/remapper/ForgeRemapper.java
@@ -0,0 +1,71 @@
+package io.github.fabriccompatibilitylayers.fabricatedforge.remapper;
+
+import io.github.fabriccompatibilitylayers.fabricatedfml.remapper.Constants;
+import io.github.fabriccompatibilitylayers.fabricatedfml.remapper.FMLMappingsConfig;
+import io.github.fabriccompatibilitylayers.modremappingapi.api.v2.*;
+import net.fabricmc.api.EnvType;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ForgeRemapper implements ModRemapper {
+    @Override
+    public String getContextId() {
+        return Constants.CONTEXT_ID;
+    }
+
+    @Override
+    public void init(CacheHandler cacheHandler) {
+
+    }
+
+    @Override
+    public List<ModDiscovererConfig> getModDiscoverers() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ModRemapper> collectSubRemappers(List<ModCandidate> list) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public MappingsConfig getMappingsConfig() {
+        return new FMLMappingsConfig();
+    }
+
+    @Override
+    public List<RemappingFlags> getRemappingFlags() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void afterRemapping() {
+
+    }
+
+    @Override
+    public void afterAllRemappings() {
+
+    }
+
+    @Override
+    public void addRemappingLibraries(List<RemapLibrary> list, EnvType envType) {
+
+    }
+
+    @Override
+    public void registerAdditionalMappings(MappingBuilder mappingBuilder) {
+
+    }
+
+    @Override
+    public void registerPreVisitors(VisitorInfos visitorInfos) {
+
+    }
+
+    @Override
+    public void registerPostVisitors(VisitorInfos visitorInfos) {
+
+    }
+}
