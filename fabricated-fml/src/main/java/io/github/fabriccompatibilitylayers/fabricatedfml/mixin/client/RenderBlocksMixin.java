adc6fc9d0d35a70669c5827973f0c6d5b5bfbed8
Cat Core <arthur.koalas@free.fr>
2026-05-31 20:34:56 +0200

    Fix Custom Block rendering in Inventory and Hand

--- fabricated-fml/src/main/java/io/github/fabriccompatibilitylayers/fabricatedfml/mixin/client/RenderBlocksMixin.java
+++ fabricated-fml/src/main/java/io/github/fabriccompatibilitylayers/fabricatedfml/mixin/client/RenderBlocksMixin.java
@@ -1,6 +1,9 @@
 package io.github.fabriccompatibilitylayers.fabricatedfml.mixin.client;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Cancellable;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.src.Block;
 import net.minecraft.src.FMLRenderAccessLibrary;
@@ -10,7 +13,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Arrays;
@@ -40,16 +42,33 @@ public class RenderBlocksMixin {
         return true;
     }
 
-    @Inject(method = "func_78600_a", at = @At("RETURN"))
-    private void fml$renderInventoryBlock(Block p_78600_1_, int p_78600_2_, float p_78600_3_, CallbackInfo ci, @Local(ordinal = 1) int var14) {
-        if (!VANILLA_MODEL_IDS.contains(var14)) {
-            FMLRenderAccessLibrary.renderInventoryBlock((RenderBlocks) (Object) this, p_78600_1_, p_78600_2_, var14);
+    @Unique
+    private static final List<Integer> VANILLA_ITEM_MODEL_IDS_INVENTORY = Arrays.asList(
+            21, 11, 27, 10, 2, 6, 22, 13, 23, 19, 1, 0, 31, 16, 26
+    );
+
+    @WrapOperation(method = "func_78600_a", at = @At(value = "INVOKE", target = "Lnet/minecraft/src/Block;func_71857_b()I", ordinal = 0))
+    private int fml$renderInventoryBlock(Block instance, Operation<Integer> original,
+                                         @Local(ordinal = 0, argsOnly = true) int p_78600_2_,
+                                         @Cancellable CallbackInfo ci) {
+        int var14 = original.call(instance);
+
+        if (!VANILLA_ITEM_MODEL_IDS_INVENTORY.contains(var14)) {
+            FMLRenderAccessLibrary.renderInventoryBlock((RenderBlocks) (Object) this, instance, p_78600_2_, var14);
+            ci.cancel();
         }
+
+        return var14;
     }
+
+    @Unique
+    private static final List<Integer> VANILLA_ITEM_MODEL_IDS = Arrays.asList(
+            0, 31, 13, 10, 11, 27, 22, 21, 16, 26
+    );
 
     @ModifyReturnValue(method = "func_78597_b", at = @At("RETURN"))
     private static boolean fml$renderItemAsFull3DBlock(boolean original, int p_78597_0_) {
-        if (!original && !VANILLA_MODEL_IDS.contains(p_78597_0_)) {
+        if (!original && !VANILLA_ITEM_MODEL_IDS.contains(p_78597_0_)) {
             return FMLRenderAccessLibrary.renderItemAsFull3DBlock(p_78597_0_);
         }
 
