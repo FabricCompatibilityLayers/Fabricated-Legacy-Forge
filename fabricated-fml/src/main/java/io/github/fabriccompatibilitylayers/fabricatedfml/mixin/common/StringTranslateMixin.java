39ce419a54878bc90b8311c60541b19fb9f00c6b
Cat Core <arthur.koalas@free.fr>
2026-06-06 21:37:57 +0200

    Fix incompatibility with OSL Localization api

--- fabricated-fml/src/main/java/io/github/fabriccompatibilitylayers/fabricatedfml/mixin/common/StringTranslateMixin.java
+++ fabricated-fml/src/main/java/io/github/fabriccompatibilitylayers/fabricatedfml/mixin/common/StringTranslateMixin.java
@@ -5,6 +5,7 @@
  */
 package io.github.fabriccompatibilitylayers.fabricatedfml.mixin.common;
 
+import com.moulberry.mixinconstraints.annotations.IfModAbsent;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import net.minecraft.src.StringTranslate;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,6 +17,7 @@ import java.util.Properties;
 
 @Mixin(StringTranslate.class)
 public class StringTranslateMixin {
+    @IfModAbsent("osl-localization")
     @Inject(method = "func_74812_a", at = @At("RETURN"))
     private void fml$loadLanguageTable(Properties p_74812_1_, String p_74812_2_, CallbackInfo ci) {
         LanguageRegistry.instance().loadLanguageTable(p_74812_1_, p_74812_2_);
---
+++ fabricated-fml/src/main/java/io/github/fabriccompatibilitylayers/fabricatedfml/mixin/osl/LocaleMixin.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) 2026 Fabric Compatibility Layer Team
+ *
+ * Licensed under the Open Software License version 3.0
+ */
+package io.github.fabriccompatibilitylayers.fabricatedfml.mixin.osl;
+
+import com.moulberry.mixinconstraints.annotations.IfModLoaded;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+import net.ornithemc.osl.localization.api.language.Language;
+import net.ornithemc.osl.localization.impl.Locale;
+import net.ornithemc.osl.localization.impl.Localization;
+import net.ornithemc.osl.resource.loader.api.resource.manager.ResourceManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+@IfModLoaded("osl-localization")
+@Mixin(Locale.class)
+public abstract class LocaleMixin {
+    @Shadow
+    protected abstract void set(String key, String translation);
+
+    @Inject(method = "init", at = @At("RETURN"))
+    private void fml$initLanguage(Map<String, String> map, Properties properties, CallbackInfo ci) {
+        if (properties != null) {
+            Language language = Localization.getLanguageManager().getSelectedLanguage();
+            LanguageRegistry.instance().loadLanguageTable(properties, language != null ? language.code() : "en_US");
+        }
+    }
+
+    @Inject(method = "reload", at = @At(value = "INVOKE", target = "Lnet/ornithemc/osl/localization/impl/Locale;setLastUpdateTime()V", remap = false), remap = false)
+    private void fml$loadLanguage(ResourceManager resourceManager, List<String> languages, CallbackInfo ci) {
+        for (String language : languages) {
+            Properties properties = new Properties();
+            LanguageRegistry.instance().loadLanguageTable(properties, language);
+
+            for (String key : properties.stringPropertyNames()) {
+                String value = properties.getProperty(key);
+                this.set(key, value);
+            }
+        }
+    }
+}
